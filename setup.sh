2a8f0bbdd39cc9b5ea34720767579aae2da62bca
Kelly Marchisio <kellymarchisio@users.noreply.github.com>
2021-11-03 11:27:40 -0400

    remove data cleaning (done in lilt now)

--- setup.sh
+++ setup.sh
@@ -2,20 +2,12 @@
 
 cd $DIR/third_party/alignment-scripts-fork 
 ./preprocess/run.sh
-
-# This issue may have been fixed in later versions of alignment-scripts (Lilt) 
-cp_clean() {
-	input=$1	
-	output=$2
-	# Clean out any non-breaking spaces.
-	cat $input | sed 's/\xC2\xA0/ /g' | sed 's/\xE2\x80\x89//g' \
-		 | sed 's/\xE3\x80\x80/ /g' > $output
-}
-
 cd $DIR
+
+mkdir -p data/train data/test
 for lang in deen roen enfr; do
 	for drt in src tgt; do
-		cp_clean third_party/alignment-scripts-fork/train/*plustest* data/train
+		cp third_party/alignment-scripts-fork/train/*plustest* data/train
 		cp third_party/alignment-scripts-fork/test/$lang.lc.$drt data/test
 	done	
 	cp third_party/alignment-scripts-fork/test/$lang.talp data/test
