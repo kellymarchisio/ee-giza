eb0f0d9af1a25696607129471e64d86c330edd6d
Kelly Marchisio <kellymarchisio@users.noreply.github.com>
2021-11-03 10:07:36 -0400

    remove moses dependency

--- local-settings.sh
+++ local-settings.sh
@@ -7,7 +7,6 @@
 export DIR=`pwd`
 export LC_ALL=en_US.UTF-8
 export TMP=$DIR/tmp
-export MOSES_DIR=$DIR/third_party/mosesdecoder
 export VECMAP=$DIR/third_party/vecmap-fork
 export LILT=$DIR/third_party/alignment-scripts-fork
 
